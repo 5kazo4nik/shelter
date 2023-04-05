a0a604d08104aedbaf53c1cbdc695e74324be43e
Oleg <alhimov.oleg2012@yandex.ru>
2023-04-06 00:18:20 +0300

    feat: add self-estimation log

--- shelter/scripts/index.js
+++ shelter/scripts/index.js
@@ -2,6 +2,10 @@ import { createCarousel } from './carousel';
 import { createPagination } from './pagination';
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log(
+    '1) Реализация burger menu на обеих страницах: +26\n2) Реализация слайдера-карусели на странице Main: +36\n3) Реализация пагинации на странице Pets: +36\n4) Реализация попап на обеих страницах: +12'
+  );
+
   setBurgerClick();
 
   if (document.querySelector('.carousel__cards')) {
